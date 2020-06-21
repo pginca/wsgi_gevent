f16bd0dd29e4917e74dd0df8a094e2d4e8f9fd54
Paulo Inça <paulo.inca@quintoandar.com.br>
2020-06-21 14:12:08 -0300

    Initial commit

---
+++ flask_app/app.py
@@ -0,0 +1,14 @@
+import os
+import requests
+from flask import Flask, request
+
+api_port = os.environ['PORT_API']
+api_url = f'http://slow_api:{api_port}/'
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    delay = float(request.args.get('delay') or 1)
+    resp = requests.get(f'{api_url}?delay={delay}')
+    return f'Hi there! {resp.text}'
